8a78e2046b594532a2023c7e40de16ef86146ef9
fray david herrera duarte <148037427+Frayherrera@users.noreply.github.com>
2024-11-27 18:41:55 -0500

    s

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,9 +1,27 @@
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+<link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+<link href="https://cdn.jsdelivr.net/npm/sweetalert2@11/dist/sweetalert2.min.css" rel="stylesheet">
+<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
             {{ __('Dashboard') }}
         </h2>
-
+        @role('Administrador')
+        <p>Este contenido solo es visible para los administradores.</p>
+        @else
+        <p>No tienes permisos para ver este contenido.</p>
+        @endrole
+        @if(session('error'))
+        <script>
+            Swal.fire({
+                icon: 'success',
+                title: 'Operación Exitosa',
+                text: "{{ session('success') }}",
+                confirmButtonText: 'Aceptar'
+            });
+        </script>
+        @endif
     </x-slot>
 
     <div class="py-12">
